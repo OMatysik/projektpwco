5659120e64ab14c7f804fc1787bdadca10ddceb1
Oskar Matysik <131363281+OMatysik@users.noreply.github.com>
2024-12-09 20:10:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,13 @@
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "select * from [dbo].[items]";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['name'] . " " . $row['description'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>
